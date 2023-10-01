2edb82d3db3f4e081ce76032f9fd2a472dc44359
fahrezaAMI <fahreza@access-mobile.com>
2023-10-01 10:20:20 +0700

    fix bug image landing page

--- app/component/HomeSection.js
+++ app/component/HomeSection.js
@@ -5,10 +5,10 @@ export default function HomeSection(){
     return(
         <main className="flex w-screen">
             <section className="w-5/12 h-[calc(100vh-200px)] flex justify-center items-center">
-                <Image src="/assets/logoalquran.png" width={410} height={457} alt=""/>
+                <img src="/assets/logoalquran.png" width={410} height={457} alt=""/>
             </section>
             <aside className="w-7/12 h-[calc(100vh-200px)] flex flex-col justify-center items-center font-Poppins">
-                <Image src="/assets/logouinpqi.png" width={210} height={94} alt="" className="mb-[27px]"/>
+                <img src="/assets/logouinpqi.png" width={210} height={94} alt="" className="mb-[27px]"/>
                 <p className="text-[32px] leading-[48px] font-[600]">Selamat Datang di LearnPQI!</p>
                 <p className="text-[20px] leading-[30px] font-[400] text-center mb-[21px]">Platform E-learning berbasis website untuk <br/> Mata Kuliah Praktikum Qiraah dan Ibadah.</p>
                 <Link id="MulaiButton" href="/SignIn" className="w-[145px] h-[43px] rounded-[10px] border-[1px] border-[#00438f] bg-[#00438f] text-[#F3F6F9] text-[20px] leading-[30px] font-[600] flex items-center justify-center">Mulai</Link>
--- app/component/LoginBrand.js
+++ app/component/LoginBrand.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LoginBrand(){
     return(
         <aside className="flex flex-col justify-center items-center w-2/5">
-            <Image src="/assets/logouinpqi.png" width={171} height={85} alt="" className="mb-[19px]"/>
+            <img src="/assets/logouinpqi.png" width={171} height={85} alt="" className="mb-[19px]"/>
             <p className="font-Poppins text-[30px] leading-[45px] font-[700] text-[#04264D]">LearnPQI</p>
         </aside>
     )
